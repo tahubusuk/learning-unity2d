df4e96edc640faff5afbd3211e49d147cab71428
tahubusuk <s8chmarc@stud.uni-saarland.de>
2023-01-30 15:35:33 +0100

    fixing animation

---
+++ Assets/AnimationControl.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class AnimationControl: MonoBehaviour
+{
+    
+    public Animator _animator;
+    private string _currentState;
+    private bool _isAttack;
+    
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _isAttack = false;
+    }
+    
+    public void PlayEvent(string events, int layer)
+    {
+        // if (_currentState == events || _isAttack) return;
+        
+        if (_currentState == events) return;
+        Debug.Log(events);
+        if (events.Contains("attack"))
+        {
+            Debug.Log("attack command");
+            // _isAttack = true;
+        }
+        _animator.Play(events, layer);
+        float attackOffset = _animator.GetCurrentAnimatorStateInfo(0).length;
+        // Invoke(nameof(AttackComplete), 0f);
+        _currentState = events;
+    }
+
+    void AttackComplete()
+    {
+        _isAttack = false;
+    }
+    
+    
+    
+}
---
+++ Assets/Event.cs
@@ -0,0 +1,19 @@
+﻿
+// public enum Event
+// {
+//     PLAYER_ATTACK_RIGHT,
+//     PLAYER_ATTACK_LEFT,
+//     PLAYER_ATTACK_BEHIND,
+//     PLAYER_ATTACK_FRONT,
+//     
+//     PLAYER_IDLE_FRONT,
+//     PLAYER_IDLE_BEHIND,
+//     PLAYER_IDLE_RIGHT,
+//     
+//     PLAYER_WALK_BEHIND,
+//     PLAYER_WALK_RIGHT,
+//     PLAYER_WALK_FRONT,
+//     
+//     PLAYER_DEATH
+//
+// }
