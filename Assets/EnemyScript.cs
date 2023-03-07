f7725f1d1f9dee327aab9c1043832df117b748fd
tahubusuk <s8chmarc@stud.uni-saarland.de>
2023-03-07 13:40:26 +0100

    enemy respawn done

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -1,34 +1,84 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
 {
     public abstract class EnemyScript: MonoBehaviour
     {
-        public int health;
+        public int initialHealth;
+        public int currentHealth;
         public Rigidbody2D rb;
         public Collider2D enemyCollider;
-        public ContactFilter2D movementFilter;
-        public List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-        public float collisionOffset = 0.05f;
         public Animator _animator;
+        public AIPath aiPath;
         public float speed;
         public float distance;
         public GameObject player;
         public float aggroDistance;
+        public Vector2 _initialPosition;
+        public float maxDistance = 3;
 
+        public void Start()
+        {
+            currentHealth = initialHealth;
+            _initialPosition = transform.position;
+        }
+
+        public void Update()
+        {
+            Debug.Log(currentHealth.ToString());
+            if (currentHealth <= 0) return;
+            Move();
+        }
+        
         public virtual void HandleUpdate()
         {
             
         }
         
-        public virtual void Move()
+        //picking random point near the spawn point
+        private Vector2 PickRandomPoint ()
         {
+            var point = _initialPosition;
+            point.y += Random.Range((float)-0.3, (float)0.3);
+            point.x += Random.Range((float)-0.3, (float)0.3);
+            return point;
+        }
+        
+        //todo check the State first then move. Make enemy spawner first. 
+        protected virtual void Move()
+        {
+            var playerPosition = player.transform.position;
+            var enemyPosition = transform.position;
+            distance = Vector2.Distance(playerPosition, enemyPosition);
             
+            //check if the position is too far
+            if (distance > maxDistance) return;
+            
+            if (aiPath.pathPending) return;
+            if (aiPath.reachedEndOfPath || !aiPath.hasPath)
+            {
+                aiPath.destination = PickRandomPoint();
+                aiPath.SearchPath();
+            }
+
+            // check if player near aggroDistance, if yes, calculate the path and move
+            if (!(distance < aggroDistance)) return;
+            aiPath.destination = playerPosition;
+            aiPath.SearchPath();
         }
 
         public virtual void TakeDamage(int damage)
         {
+            currentHealth -= damage;
+            if (currentHealth <= 0)
+            {
+                _animator.SetTrigger("Destroy");
+            }
             
         }
 
@@ -40,6 +90,16 @@ namespace DefaultNamespace
         public virtual void Defeated()
         {
             
+            _animator.SetTrigger("WaitToRespawn");
+            StartCoroutine(Respawn());
+        }
+
+
+        private IEnumerator Respawn()
+        {
+            yield return new WaitForSeconds(5f);
+            Instantiate(gameObject, _initialPosition, Quaternion.identity);
+            Destroy(gameObject);
         }
         
     }
--- Assets/SlimeScript.cs
+++ Assets/SlimeScript.cs
@@ -3,72 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using DefaultNamespace;
 using Pathfinding;
+using TMPro.Examples;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class SlimeScript : EnemyScript
 {
     // Start is called before the first frame update
-    public AIPath aiPath;
 
-    private Vector2 _initialPosition;
-
-    private void Start()
-    {
-        _initialPosition = transform.position;
-    }
+    // public void Start()
+    // {
+    // }
 
     // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
+    // public void Update()
+    // {
+    // }
     
-    //picking random point near the spawn point
-    private Vector2 PickRandomPoint ()
-    {
-        var point = _initialPosition;
-        point.y += Random.Range((float)-0.3, (float)0.3);
-        point.x += Random.Range((float)-0.3, (float)0.3);
-        return point;
-    }
-
-
-    public override void Move()
-    {
-        if (aiPath.pathPending) return;
-        if (aiPath.reachedEndOfPath || !aiPath.hasPath)
-        {
-            aiPath.destination = PickRandomPoint();
-            aiPath.SearchPath();
-        }
-
-        // check if player near aggroDistance, if yes, calculate the path and move
-        var playerPosition = player.transform.position;
-        var enemyPosition = transform.position;
-        distance = Vector2.Distance(playerPosition, enemyPosition);
-        if (!(distance < aggroDistance)) return;
-        aiPath.destination = playerPosition;
-        aiPath.SearchPath();
-    }
-
     public override void Attack()
     {
         //todo implement attack -> casting collision to player karna slime lompat
         base.Attack();
     }
 
-    public override void TakeDamage(int damage)
-    {
-        health -= damage;
-        if (health <= 0)
-        {
-            _animator.SetTrigger("Destroy");
-        }
-    }
 
-    public override void Defeated()
-    {
-        Destroy(gameObject);
-    }
 }
